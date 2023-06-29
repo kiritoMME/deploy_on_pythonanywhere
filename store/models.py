41329e21dd06af9f736dcedf1e8924698725f5fa
kiritoMME <77681921+kiritoMME@users.noreply.github.com>
2023-06-29 21:14:19 +0300

    Upload The main Design

---
+++ store/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000000)
+    sizes = models.CharField(max_length=100)
+    price = models.FloatField(default=0)
+    rate = models.FloatField(default=5)
+    rate_text = models.CharField(max_length=5,default='FFFFF')
+    featured_product = models.BooleanField(default=False)
+    offer = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='static/images/', default='static/images/product-null.jpg' ,blank=True, null=True)
+    gallery_1 = models.ImageField(upload_to='static/images/',default='static/images/product-null.jpg' ,blank=True, null=True)
+    gallery_2 = models.ImageField(upload_to='static/images/',default='static/images/product-null.jpg' ,blank=True, null=True)
+    gallery_3 = models.ImageField(upload_to='static/images/',default='static/images/product-null.jpg' ,blank=True, null=True)
+    gallery_4 = models.ImageField(upload_to='static/images/',default='static/images/product-null.jpg' ,blank=True, null=True)
+    offer_image = models.ImageField(upload_to='static/images/',blank=True, null=True)
+
+class testimonial(models.Model):
+    name = models.CharField(max_length=500)
+    comment = models.CharField(default="this is from django default comment elit. Quis, repellat. Id natus aut, quasi unde voluptatum, ipsa fugit esse enim numquam officiis cupiditate repellendus nihil fuga. A iure fugit hic. lo",max_length=1000000)
+    rate_text = models.CharField(max_length=5,default='FFFFF')
+    image = models.ImageField(upload_to='static/images/',blank=True, null=True)
+
+class order(models.Model):
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    product = models.ForeignKey(product, default=None, on_delete=models.CASCADE)
+    size = models.CharField(max_length=100)
+    count = models.IntegerField()
+    is_confirmed = models.BooleanField(default=False)
+    # confirmed_order = models.ForeignKey(confirmed_orders)
+
+class confirmed_orders(models.Model):
+    user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100000, default="")
+    phone = models.CharField(max_length=15, default="")
+    total_price = models.FloatField()
+    orders = models.ManyToManyField(order)
+    orders2 = models.ManyToManyRel(orders, order)
+    status = models.CharField(default='pending', max_length=20)
---
+++ store/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('home', views.toindex, name='index'),
+    path('index', views.toindex, name='index'),
+    path('product_details/<str:id>', views.product_details, name='product_details'),
+    path('product_admin_details/<str:id>', views.product_admin_details, name='product_admin_details'),
+    path('product_admin_details/view/<str:id>', views.product_admin_details_view, name='product_admin_details_view'),
+    path('edit_product/<str:id>', views.edit_product, name='edit_product'),
+    path('delete_product/<str:id>', views.delete_product, name='delete_product'),
+    path('products', views.products, name='products'),
+    path('products_admin', views.products_admin, name='products_admin'),
+    path('cart', views.cart, name='cart'),
+    path('addtocart/<str:product_id>', views.addtocart, name='addtocart'),
+    path('removeorder/<str:product_id>', views.removeorder, name='removeorder'),
+    path('account/signup', views.account_signup, name='account_signup'),
+    path('account/login', views.account_login, name='account_login'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('confirm', views.confirm, name='confirm'),
+    path('send', views.send, name='send'),
+    path('confirmed', views.confirmed, name='confirmed'),
+    path('confirmed_admin', views.confirmed_admin, name='confirmed_admin'),
+    path('edit_confirmed_order/<str:id>/<str:stat>', views.edit_confirmed_order, name='edit_confirmed_order'),
+    path('control_panel', views.control_panel, name='control_panel'),
+    path('message/<str:message>/<str:directlink>', views.message_template, name='message_template'),
+]
